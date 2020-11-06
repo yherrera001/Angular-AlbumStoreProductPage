e0d5660314556202c1436c10c8f844205f7e4e31
yherrera001 <yherrera001@my.wilmu.edu>
2020-11-05 22:20:36 -0500

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,12 +8,13 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 albumInfo;
-  constructor(private _productService) { }
+
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(
       response => this.albumInfo = response
-    )
+    );
   }
 
 }
