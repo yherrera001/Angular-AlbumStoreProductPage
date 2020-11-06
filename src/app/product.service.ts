2edbcb20e23438092041c3fe75f2ce7745d790fe
yherrera001 <yherrera001@my.wilmu.edu>
2020-11-05 22:23:22 -0500

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
