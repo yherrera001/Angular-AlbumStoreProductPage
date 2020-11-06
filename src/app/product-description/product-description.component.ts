f1630b2f3ae9270d36a553dea92914e8d1658268
yherrera001 <yherrera001@my.wilmu.edu>
2020-11-05 22:36:04 -0500

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Album } from 'app/album';
+import { Album } from '../album';
 import { ProductService } from '../product.service';
 
 @Component({
